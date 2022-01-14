4ab9859834e0b8178e1d85a4ec603dd71c9eb349
e406hsy <e406hsy@gmail.com>
2022-01-14 20:43:36 +0900

    fix: awt exception 해결

--- src/main/kotlin/com/soonyong/customdeck/server/CustomDeckServerApplication.kt
+++ src/main/kotlin/com/soonyong/customdeck/server/CustomDeckServerApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class CustomDeckServerApplication
 
 fun main(args: Array<String>) {
+    System.setProperty("java.awt.headless","false")
     runApplication<CustomDeckServerApplication>(*args)
 }
